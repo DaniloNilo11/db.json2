77133b2f444fde3047f1a429cfb39a8f79bef54b
Danilo Cardoso Custodio <114489699+DaniloNilo11@users.noreply.github.com>
2025-09-17 08:14:18 -0300

    Add files via upload

---
+++ Example-sources/com/example/Adicionai.java
@@ -0,0 +1,34 @@
+
+package com.example;
+
+import javax.annotation.Generated;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+@Generated("jsonschema2pojo")
+public class Adicionai {
+
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("email")
+    @Expose
+    private String email;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
